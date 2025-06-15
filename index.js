2005b04a16dfa0e8e0d38e78dcfb847fddd72b5c
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-06-15 23:59:53 +0600

    problem

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['https://mathmatter-by-shahrin-tarin.web.app'],
+  origin: ['https://mathmatter-by-shahrin-tarin.web.app','http://localhost:5174'],
   credentials: true
 }));
 app.use(express.json())
